9938d2d3d81b274ffc1cfce0f2a71c7440804454
Violet <ziyxiang@stanford.edu>
2024-07-18 22:30:09 -0700

    GoEmotions_sentiment set-2 data

--- data/download_fb_data.py
+++ data/download_fb_data.py
@@ -67,7 +67,8 @@ if __name__ == '__main__':
     # current_data_prefix = 'Full-iSarcasm-1'
     # current_data_prefix = 'Full-iSarcasm-2'
     # current_data_prefix = 'Full-iSarcasm-3'
-    current_data_prefix = 'Full-GoEmotions_Sentiment-1'
+    # current_data_prefix = 'Full-GoEmotions_Sentiment-1'
+    current_data_prefix = 'Full-GoEmotions_Sentiment-2'
 
     attention_check_strs = list(ATTENTION_CHECK_QA.keys())
     for subject_id in collection_names:
