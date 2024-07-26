8f2b89dca7e9ea85fccbb41a986ab6c884a64018
Violet <ziyxiang@stanford.edu>
2024-07-25 23:16:00 -0700

    CSTANCE - set 1

--- src/App-Politeness.js
+++ src/App-Politeness.js
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import Papa from 'papaparse';
 import { collection, addDoc } from 'firebase/firestore';
-import { db } from './firebaseConfig'; // 导入Firebase配置
-import Instructions from './Instructions'; // 导入Instructions组件
-import Demographics from './Demographics'; // 导入Demographics组件
+import { db } from './firebaseConfig'; // Import Firebase config
+import Instructions from './Instructions'; // Import the Instructions component
+import Demographics from './Demographics'; // Import the Demographics component
 import './App.css';
 
-// 加载CSV文件的函数
+// Function to load the CSV file
 const loadCSV = (url) => {
   return new Promise((resolve, reject) => {
     Papa.parse(url, {
@@ -22,7 +22,7 @@ const loadCSV = (url) => {
   });
 };
 
-// 使用Fisher-Yates算法打乱数组的函数
+// Function to shuffle an array using Fisher-Yates algorithm
 const shuffleArray = (array) => {
   const shuffledArray = [...array];
   for (let i = shuffledArray.length - 1; i > 0; i--) {
@@ -33,20 +33,20 @@ const shuffleArray = (array) => {
 };
 
 const attentionChecks = [
-  { question: "请确定以下陈述是正确还是错误的。", statement: "1 + 1 = 2", note: "", correctAnswer: "正确", isAttentionCheck: true },
-  { question: "请确定以下陈述是正确还是错误的。", statement: "玛丽对她的假期感到兴奋，但由于工作不得不取消。玛丽对这种情况可能感到兴奋。", note: "", correctAnswer: "错误", isAttentionCheck: true },
-  { question: " ", statement: "请选择‘错误’", note: "", correctAnswer: "错误", isAttentionCheck: true },
-  { question: "请确定以下陈述是正确还是错误的。", statement: "约翰认为疫苗在预防疾病方面是有效的。约翰可能会支持疫苗接种计划。", note: "", correctAnswer: "正确", isAttentionCheck: true }
+  { question: "Please determine if the following statement is true or false.", statement: "1 + 1 = 2", note: "", correctAnswer: "True", isAttentionCheck: true },
+  { question: "Please determine if the following statement is true or false.", statement: "Mary was excited about her vacation, but had to cancel it due to work. Mary is likely to feel excited about this situation.", note: "", correctAnswer: "False", isAttentionCheck: true },
+  { question: " ", statement: "Please select 'False'", note: "", correctAnswer: "False", isAttentionCheck: true },
+  { question: "Please determine if the following statement is true or false.", statement: "John believes vaccines are effective at preventing diseases. John is likely to support vaccination programs.", note: "", correctAnswer: "True", isAttentionCheck: true }
 ];
 
 function App() {
-  // 说明内容
+  // instruction content
   const [showInstructions, setShowInstructions] = useState(true);
   const [prolificID, setProlificID] = useState('');
-  // 主研究内容
+  // main study content
   const [questions, setQuestions] = useState([]);
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
-  // 人口统计内容
+  // demographics content
   const [showDemographics, setShowDemographics] = useState(false);
   const [responses, setResponses] = useState([]);
 
@@ -55,11 +55,11 @@ function App() {
   };
 
   useEffect(() => {
-    loadCSV('data/set_1.csv')
+    loadCSV('data/set_3.csv')
       .then((data) => {
-        const questionsData = data.filter(item => item.original_data !== undefined && item.original_data.trim() !== '')
-        .map(item => ({
-          question: `当一个人在对话中说"${item.original_data}"时，他对于这个话题："${item.label_type}"的态度是,`,
+        const questionsData = data.filter(item => item.original_data !== undefined && item.original_data.trim() !== '').
+        map(item => ({
+          question: `Was the person intended to be polite when they said "${item.original_data}" in the conversation?`,
           statement: item.conversation,
           note: item.note || '',
           isAttentionCheck: false
@@ -68,7 +68,7 @@ function App() {
         setQuestions(allQuestions);
       })
       .catch((error) => {
-        console.error('加载CSV时出错:', error);
+        console.error('Error loading CSV:', error);
       });
   }, []);
 
@@ -81,7 +81,7 @@ function App() {
   };
 
   const logResponse = async (response) => {
-    console.log('记录响应:', response);
+    console.log('Logging response:', response);
     try {
       const currentQuestion = questions[currentQuestionIndex];
       const newResponse = {
@@ -90,41 +90,41 @@ function App() {
         response: response,
         timestamp: new Date(),
       };
-      let updatedProlificID = `SIMS-ch-1-${prolificID}`;
-      await addDoc(collection(db, updatedProlificID), newResponse);
-      console.log('响应已记录:', response);
+      let updatedProlificID = `Full-Politeness-3-${prolificID}`;
+      await addDoc(collection(db, updatedProlificID), newResponse);      
+      console.log('Response logged:', response);
 
-      // 确保在记录响应后调用此函数
+      // Ensure this is called after logging the response
       handleNextQuestion();
     } catch (e) {
-      console.error('添加文档时出错: ', e);
+      console.error('Error adding document: ', e);
     }
   };
 
   const handleDemographicsComplete = async (demographicsData) => {
-    // 处理人口统计调查的完成
-    console.log('人口统计调查已完成');
+    // Handle completion of the demographics survey
+    console.log('Demographics survey completed');
   };
 
   const parseConversation = (conversation) => {
     return conversation.split('\n').map((line, index) => {
       const trimmedLine = line.trim();
-      if (trimmedLine.startsWith('A：')) {
+      if (trimmedLine.startsWith('A:')) {
         return { speaker: 'A', content: trimmedLine.substring(2).trim() };
-      } else if (trimmedLine.startsWith('B：')) {
+      } else if (trimmedLine.startsWith('B:')) {
         return { speaker: 'B', content: trimmedLine.substring(2).trim() };
       }
       return null;
-    }).filter(Boolean);    
+    }).filter(Boolean);
   };
 
-  const currentQuestion = questions[currentQuestionIndex]?.question || '加载中...';
+  const currentQuestion = questions[currentQuestionIndex]?.question || 'Loading...';
   const currentStatement = questions[currentQuestionIndex]?.statement || '';
   const currentNote = questions[currentQuestionIndex]?.note || '';
   const isAttentionCheck = questions[currentQuestionIndex]?.isAttentionCheck || false;
   const parsedConversation = parseConversation(currentStatement);
-  
-  return (    
+
+  return (
     <div className="App">
       {showInstructions ? (
         <Instructions onComplete={handleInstructionsComplete} setProlificID={setProlificID} />
@@ -132,48 +132,42 @@ function App() {
         <Demographics
           onComplete={handleDemographicsComplete}
           responses={responses}
-          ratings={[]} // 假设您有需要传递的评分
+          ratings={[]} // Assuming you have ratings to pass here
           prolificID={prolificID}
         />
       ) : (
         <header className="App-header">
-          {!isAttentionCheck && <p>请阅读下面的对话：</p>}
-          <div className="conversation">            
-            {!isAttentionCheck && parsedConversation.map((line, index) => (              
+          {!isAttentionCheck && <p>Please read the conversation below:</p>}
+          <div className="conversation">
+            {!isAttentionCheck && parsedConversation.map((line, index) => (
               <p key={index} style={{ color: line.speaker === 'A' ? 'red' : 'blue', margin: '0 0 10px 0' }}>
                 {line.speaker}: {line.content}
               </p>
-            ))}            
-          </div>          
+            ))}
+            {isAttentionCheck && <p>{currentStatement}</p>}
+          </div>
           <p>{currentQuestion}</p>
-          {isAttentionCheck && <p>{currentStatement}</p>}
           {currentNote && <p>{currentNote}</p>}
           <div>
             {isAttentionCheck ? (
               <>
                 <button className="App-link" style={{ marginRight: '25px' }} onClick={() => logResponse('True')}>
-                  正确
+                  True
                 </button>
                 <button className="App-link" style={{ marginRight: '25px' }} onClick={() => logResponse('False')}>
-                  错误
-                </button>
-                <button className="App-link" onClick={() => logResponse('Ambiguous')}>
-                  模棱两可
+                  False
                 </button>
               </>
             ) : (
               <>
-                <button className="App-link" style={{ marginRight: '25px' }} onClick={() => logResponse('positive')}>
-                  积极的
+                <button className="App-link" style={{ marginRight: '25px' }} onClick={() => logResponse('Polite')}>
+                  Polite: The speaker intended to be polite
                 </button>
-                <button className="App-link" style={{ marginRight: '25px' }} onClick={() => logResponse('negative')}>
-                  消极的
-                </button>               
-                <button className="App-link" style={{ marginRight: '25px' }} onClick={() => logResponse('neutral')}>
-                  中立的
+                <button className="App-link" style={{ marginRight: '25px' }} onClick={() => logResponse('Impolite')}>
+                  Impolite: The speaker intended to be impolite
                 </button>
-                <button className="App-link" style={{ marginRight: '25px' }} onClick={() => logResponse('ambiguous')}>
-                  模棱两可
+                <button className="App-link" onClick={() => logResponse('Ambiguous')}>
+                  Ambiguous: I am not sure if the speaker intended to be polite or impolite
                 </button>
               </>
             )}
--- src/Instructions-Politeness.js
+++ src/Instructions-Politeness.js
@@ -57,10 +57,12 @@ const Instructions = ({ onComplete, setProlificID }) => {
     setProlificID(id);
   };
 
+
   const handleComprehensionChange = () => {
-    console.log("理解测试是否有效？", isComprehensionValid);
+    console.log("Is comprehension valid?", isComprehensionValid);
     setIsComprehensionValid(validateComprehensionTest());
   };
+
 
   const validateComprehensionTest = () => {
     const correctAnswers = {
@@ -72,10 +74,10 @@ const Instructions = ({ onComplete, setProlificID }) => {
     for (const question in correctAnswers) {
       const selectedAnswer = document.querySelector(`input[name="${question}"]:checked`);
       if (!selectedAnswer) {
-        console.log(`未选择${question}的答案`);
+        console.log(`No answer selected for ${question}`);
         return false;
       }
-      console.log(`${question}的选择答案: ${selectedAnswer.value}`);
+      console.log(`Selected answer for ${question}: ${selectedAnswer.value}`);
       if (selectedAnswer.value !== correctAnswers[question]) {
         return false;
       }
@@ -89,8 +91,8 @@ const Instructions = ({ onComplete, setProlificID }) => {
       id: 'intro0',
       content: (
         <>
-          <h2>同意书</h2>
-          <p>您的数据将用于一个研究项目的分析，并将在一篇以完全匿名化方式发表的文章中使用。</p>
+          <h2>Consent Form</h2>
+          <p> Your data will be used for analysis in a research project and will be used in a publication in a fully anonymized manner.</p>
           <div className="form-check">
             <input
               className="form-check-input"
@@ -100,7 +102,7 @@ const Instructions = ({ onComplete, setProlificID }) => {
               onChange={handleConsentChange}
             />
             <label className="form-check-label" htmlFor="consent-checkbox">
-              我已阅读并理解上述信息，并同意参与此研究。
+              I have read and understood the information above, and I agree to participate in this study.
             </label>
           </div>
         </>
@@ -111,12 +113,12 @@ const Instructions = ({ onComplete, setProlificID }) => {
       content: (
         <>
           <h2>Prolific ID</h2>
-          <p>请输入您的Prolific ID以继续：</p>
+          <p>Please enter your Prolific ID to continue:</p>
           <input
             type="text"
             value={prolificID}
             onChange={handleProlificIDChange}
-            placeholder="请输入您的Prolific ID ..."
+            placeholder="Enter your Prolific ID [if you are testing the exp just insert 1]"
             className="form-control"
             required
           />
@@ -127,15 +129,15 @@ const Instructions = ({ onComplete, setProlificID }) => {
       id: 'intro2', 
       content: (
         <>
-          <h2>说明 1/2：简介</h2> 
-          <p>欢迎参加此情感识别实验！</p>
+          <h2>Instructions 1/2: Introduction</h2> 
+          <p>Welcome to this intent recognition experiment!</p>
           <p>
-            在这项研究中，您将会看到一个话题和一段对话，其中包含一个陈述。您的任务是识别该陈述的想法是
-            <span style={{color: 'blue'}}>反对</span>、
-            <span style={{color: 'red'}}>支持</span> 还是 <span style={{color: 'green'}}>中立</span>。
+            In this study, you will be presented a conversation containing a statement and your task is to 
+            judge if the statement is <span style={{color: 'blue'}}> polite </span> 
+            or <span style={{color: 'red'}}> impolite </span> in the context of the conversation
           </p>
-          <p>我们的目标是了解人们在多大程度上能够识别陈述背后的立场。</p>
-          <p><strong>重要提示：</strong>请仔细阅读所有说明。在说明的最后，您将被要求回答理解问题，以确保您完全理解了任务和您在此实验中的角色。</p>
+          <p> Our goal is to find out how well people can recognize the intent behind a statement in a conversation. </p>
+          <p><strong>IMPORTANT NOTE:</strong> Please read all instructions carefully and thoroughly. At the end of the instructions, you will be asked comprehension questions to ensure you have fully understood the task and your role in this experiment.</p>
         </>
       ) 
     },
@@ -143,12 +145,11 @@ const Instructions = ({ onComplete, setProlificID }) => {
       id: 'intro3', 
       content: (
         <>
-          <h2>说明 2/2：选择</h2>
-          <p>对于每个陈述，您将从以下选项中选择：</p>
-          <p><strong>反对</strong></p>
-          <p><strong>支持</strong></p>
-          <p><strong>中立</strong></p>
-          <p><strong>模糊</strong>：如果在没有更多背景信息的情况下无法判断陈述是支持还是反对堕胎，或者如果您不理解该陈述。</p>
+          <h2>Instructions 2/2: Choices</h2>
+          <p>For each statement, you will choose from the following:</p>
+          <p><strong>Polite</strong>: If the statement is polite given the conversation.</p>
+          <p><strong>Impolite</strong>: If the statement is impolite given the conversation.</p>
+          <p><strong>Ambiguous</strong>: If you cannot determine if the statement is polite or not, or if you do not understand the statement given the conversation.</p>
         </>
       ) 
     },
@@ -156,43 +157,43 @@ const Instructions = ({ onComplete, setProlificID }) => {
       id: 'intro4',
       content: (
         <>
-          <h2>理解测验</h2>
+          <h2>Comprehension Quiz</h2>
           <form onChange={handleComprehensionChange}>
             <div className="question">
-              <p>1. 本实验的主要目的是识别陈述的情感是积极的、消极的还是中立的。</p>
+              <p>1. The main objective of this experiment is to identify if the sentiment of a statement is positive or negative.</p>
               <div className="form-check">
                 <input type="radio" id="comp-1-true" name="comprehension-1" value="true" className="form-check-input" />
-                <label htmlFor="comp-1-true" className="form-check-label">正确</label>
+                <label htmlFor="comp-1-true" className="form-check-label">True</label>
               </div>
               <div className="form-check">
                 <input type="radio" id="comp-1-false" name="comprehension-1" value="false" className="form-check-input" />
-                <label htmlFor="comp-1-false" className="form-check-label">错误</label>
+                <label htmlFor="comp-1-false" className="form-check-label">False</label>
               </div>
             </div>
             <div className="question">
-              <p>2. 当您不理解陈述时，只需选择“中立”作为答案。</p>
+              <p>2. When you do not understand the statement, just select "impolite" as the answer.</p>
               <div className="form-check">
                 <input type="radio" id="comp-2-true" name="comprehension-2" value="true" className="form-check-input" />
-                <label htmlFor="comp-2-true" className="form-check-label">正确</label>
+                <label htmlFor="comp-2-true" className="form-check-label">True</label>
               </div>
               <div className="form-check">
                 <input type="radio" id="comp-2-false" name="comprehension-2" value="false" className="form-check-input" />
-                <label htmlFor="comp-2-false" className="form-check-label">错误</label>
+                <label htmlFor="comp-2-false" className="form-check-label">False</label>
               </div>
             </div>
             <div className="question">
-              <p>3. 当您认为陈述的情感是模棱两可的，并且在没有更多上下文的情况下无法确定时，您应该选择“模糊”。</p>
+              <p>3. When you think the statement is ambiguous and can be either polite or impolite, you should select "ambiguous". </p>
               <div className="form-check">
                 <input type="radio" id="comp-3-true" name="comprehension-3" value="true" className="form-check-input" />
-                <label htmlFor="comp-3-true" className="form-check-label">正确</label>
+                <label htmlFor="comp-3-true" className="form-check-label">True</label>
               </div>
               <div className="form-check">
                 <input type="radio" id="comp-3-false" name="comprehension-3" value="false" className="form-check-input" />
-                <label htmlFor="comp-3-false" className="form-check-label">错误</label>
+                <label htmlFor="comp-3-false" className="form-check-label">False</label>
               </div>
             </div>
             {showComprehensionError && (
-              <p style={{ color: 'red' }}>有一个理解问题回答不正确。请仔细检查答案！</p>
+              <p style={{ color: 'red' }}>One of the comprehension is incorrect. Please double check the answers!</p>
             )}
           </form>
         </>
@@ -218,7 +219,7 @@ const Instructions = ({ onComplete, setProlificID }) => {
       <div className="text-center mt-4">
         {currentPage > 0 && (
           <button id="prev-btn" className="btn btn-secondary" onClick={handlePrev}>
-            上一页
+            Previous
           </button>
         )}
         <button
@@ -227,7 +228,7 @@ const Instructions = ({ onComplete, setProlificID }) => {
           onClick={handleNext}
           disabled={(currentPage === 0 && !isConsentChecked) || (currentPage === 1 && !prolificID) || (currentPage === 4 && !isComprehensionValid)}
         >
-          {currentPage === totalPages - 1 ? '完成' : '下一页'}
+          {currentPage === totalPages - 1 ? 'Complete' : 'Next'}
         </button>
       </div>
     </div>
