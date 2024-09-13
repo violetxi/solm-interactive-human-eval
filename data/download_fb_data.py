4f0978ca5f1773e9b3cc6ae4ac795450c6f37f0d
Violet <ziyxiang@stanford.edu>
2024-09-13 05:27:02 -0700

    GoEmotions: set 5 data & set 6 exp

--- data/download_fb_data.py
+++ data/download_fb_data.py
@@ -86,7 +86,8 @@ if __name__ == '__main__':
     # current_data_prefix = 'Full-GoEmotions_Sentiment-1'
     # current_data_prefix = 'Full-GoEmotions_Sentiment-2'
     # current_data_prefix = 'Full-GoEmotions_Sentiment-3'
-    current_data_prefix = 'Full-GoEmotions_Sentiment-4'
+    # current_data_prefix = 'Full-GoEmotions_Sentiment-4'
+    current_data_prefix = 'Full-GoEmotions_Sentiment-5'
 
     # current_data_prefix = 'Full-SemT6_Abortion-1'
     # current_data_prefix = 'Full-SemT6_Abortion-2'
