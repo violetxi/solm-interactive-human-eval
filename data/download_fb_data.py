642ee162250fe64a7a234883b2c8e6736c1d7ac9
Violet <ziyxiang@stanford.edu>
2024-09-30 20:50:06 -0700

    GoEmo - ms s2 data & s3 exp

--- data/download_fb_data.py
+++ data/download_fb_data.py
@@ -90,7 +90,8 @@ if __name__ == '__main__':
     # current_data_prefix = 'Full-GoEmotions_Sentiment-4'
     # current_data_prefix = 'Full-GoEmotions_Sentiment-5'
     # current_data_prefix = 'Full-GoEmotions_Sentiment-6'
-    current_data_prefix = 'Full-GoEmotions_Sentiment-7'
+    # current_data_prefix = 'Full-GoEmotions_Sentiment-7'
+    current_data_prefix = 'Full-GoEmotions_Sentiment-8'
 
     # # Abortion
     # current_data_prefix = 'Full-SemT6_Abortion-1'
